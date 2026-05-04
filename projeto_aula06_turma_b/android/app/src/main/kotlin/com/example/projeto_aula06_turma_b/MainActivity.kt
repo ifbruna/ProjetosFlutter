cd229ef23cadf0100141d8f559a0d37750ced739
JanRafaelSilva <20251ctb0100011@estudantes.ifpr.edu.br>
2026-05-04 11:30:42 -0300

    aula0405

---
+++ projeto_aula06_turma_b/android/app/src/main/kotlin/com/example/projeto_aula06_turma_b/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projeto_aula06_turma_b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ projeto_aula06_turma_b/lib/receita.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class Receita extends StatelessWidget {
+  const Receita({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.sizeOf(context);
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Receita de Bolo de Cenoura'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Imagem da Receita
+            SizedBox(
+              height: size.height * 0.3,
+              child: Image.asset(
+                'assets/images/bolo_cenoura.jpg', // Verifique o caminho de imagem
+                fit: BoxFit.cover,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Pontuação da Receita (Row de estrelas)
+            Row(
+              children: [
+                for (int i = 0; i < 4; i++)
+                  Icon(Icons.star, color: colorScheme.primary, size: 24),
+                Icon(
+                  Icons.star_half,
+                  color: colorScheme.primary,
+                  size: 24,
+                ), // Meia estrela
+                SizedBox(width: 8),
+                Text('4.5 (250 avaliações)', style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            const SizedBox(height: 10),
+
+            Section(title: "ingredientes:",
+            itens: ["Cenouras, ovos, óleo, açúcar, farinha, fermento", 
+            "cobertura: açúcar, chocolate em pó, manteiga"],),
+
+            SizedBox(height: 20),
+
+            // Seção do Modo de Preparo
+            Section(title: "Modo de preparo:",
+            itens: ["Bata cenoura, ovos e oleo no liquidificador.", 
+            "Misture os liquidos com açúcar e farinha. Adicione o fermento por ultimo.", 
+            "Asse em forno medio (180) po9r 30-40 minutos.", 
+            "Para cobertura: cozinhe todos os ingredientes em fogo baixo ate engrossar. Despeje sobre o bolo quente."])
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+class Section extends StatelessWidget {
+  const Section({super.key, required this.title, required this.itens});
+
+  final String title;
+  final List<String> itens;
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return // Seção de Ingredientes
+      Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: colorScheme.tertiary,
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+
+                // Lista de items
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    for(var item in itens)
+                    Item(text: item,
+                    icon: Icons.square, ),
+
+                  ]
+                ),
+              ],
+            );
+  }
+}
+
+class Item extends StatelessWidget {
+  const Item({super.key, required this.text, required this.icon});
+
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+            children: [
+              Icon(icon,
+              size: 8,
+              color: Theme.of(context).colorScheme.primary),
+              SizedBox(width: 4,),
+              Expanded(child: 
+              Text(text)),
+              ],
+            );
+  }
+}
+
